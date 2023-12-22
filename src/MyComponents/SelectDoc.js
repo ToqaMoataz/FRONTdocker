d3375343e448bf14447b85ce75287fd42168f55a
ToqaMoataz <150075342+ToqaMoataz@users.noreply.github.com>
2023-12-22 21:16:49 +0200

    Update SelectDoc.js

--- src/MyComponents/SelectDoc.js
+++ src/MyComponents/SelectDoc.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+function selected(name)
+{
+  return name;
+}
 function ChooseDoc()
 {
     const [docs, setDocs] = useState([]);
@@ -8,7 +12,8 @@ function ChooseDoc()
     useEffect(() => {
       const fetchData = async () => {
         try {
-          const response = await axios.get('/Doc.json');
+          const apiUrl = process.env.REACT_APP_API_URL;
+          const response = await axios.get('${apiUrl}/patient/doctor/');
           setDocs(response.data);
         } catch (error) {
           console.error('Error fetching data:', error);
@@ -33,7 +38,7 @@ function ChooseDoc()
           <tr key={index}>
             <td>{docs.Doc}</td>
             <td>{docs.speciality}</td>
-            <button ><a href='/ShowAppointment'>Select</a></button>
+            <button onClick={selected(docs.Doc)}><a href='/ShowAppointment'>Select</a></button>
           </tr>
         ))}
       </tbody>
